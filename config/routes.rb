5e20785e5a22d2f11d960ff1e7a721daaec71b1a
tsukasaki6805 <kohei.tsuka46@gmail.com>
2022-02-06 09:50:58 +0000

    route admin.public

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+root 'homes#top'
+  get 'homes/about' => 'homes#about'
+
+namespace :public do #namespace :customersをpublicに変更しました。
+end
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+namespace :admin do
+end
+
 end
