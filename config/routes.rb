7001b9f947c9e1df9230dfb90ae35e558765fdc6
tsukasaki6805 <kohei.tsuka46@gmail.com>
2022-02-08 05:20:15 +0000

    route user.game.unsubscribe.withdraw追加

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,22 @@ root 'homes#top'
   get 'homes/about' => 'homes#about'
 
 namespace :public do #namespace :usersをpublicに変更しました。
+
+  resource :users, only: [:show, :update, :edit] do
+      # 退会確認画面
+      get '/users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+      # 論理削除用のルーティング
+      patch '/users/withdrawal' => 'users#withdrawal', as: 'withdrawal'
+    end
+
+  resources :games, only: [:index, :show]
+
+  get 'users/unsubscribe' => 'users#unsubscribe'
+  patch 'users/withdraw' => 'users#withdraw'
+
 end
+
+
 
 # 管理者用
 # URL /admin/sign_in ...
