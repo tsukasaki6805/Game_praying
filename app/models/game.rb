9eb8f91281051ed4d1f3903c5eb76ef8675630ac
tsukasaki6805 <kohei.tsuka46@gmail.com>
2022-02-10 06:31:41 +0000

    game show/edit/index/_index/_newform/_item/モデル/dbマイグレイト

---
+++ app/models/game.rb
@@ -0,0 +1,15 @@
+class Game < ApplicationRecord
+
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+	#presence trueは空欄の場合を意味する。
+	belongs_to :user
+	has_many :favorites, dependent: :destroy
+    def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    end
+	has_many :game_comments, dependent: :destroy
+	validates :title, :body, presence: true
+	validates :body, length: {maximum: 200}
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :games, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :game_comments, dependent: :destroy
+  attachment :profile_image
+
 end
