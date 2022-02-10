be808bade09edcf85e75eb51a919f7d62bbb53c8
tsukasaki6805 <kohei.tsuka46@gmail.com>
2022-02-10 05:25:49 +0000

    gameコントローラー追加

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,2 +1,54 @@
 class Public::GamesController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    @game = Game.find(params[:id])
+    @game_new = Game.new
+    @user = @game.user
+    @comment = GameComment.new
+    @game_comments = @game.public_game_comments
+  end
+
+  def index
+    @games = Game.all
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user_id = current_user.id
+    if @game.save
+      redirect_to @game, notice: "You have created book successfully."
+    else
+      flash[:danger] = @game.errors.full_messages
+      @games = Game.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @game = Game.find(params[:id])
+    if current_user.id != @game.user_id
+      redirect_to games_path
+    end
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    if @game.update(game_params)
+      redirect_to public_game_path(@game), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def delete
+    @game = Game.find(params[:id])
+    @game.destoy
+    redirect_to games_path
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :body)
+  end
 end
